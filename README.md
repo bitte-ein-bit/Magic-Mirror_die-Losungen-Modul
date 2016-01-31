1051bc1a7d8389097926aed88a001c0bea6b701d
Jonathan Vogt <code@v-o-g-t.net>
2016-01-31 23:47:31 +0100

    README and Error Handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Magic-Mirror_die-Losungen-Modul
-Modul für Magic Mirror das die Losungen anzeigt
+Modul für Magic Mirror das die Losungen anzeigt.
+
+Es werden im Wechsel der Losungs und der Lehrtext angezeigt.
+
+Dieses Modul enthält aus Copyrightgründen nicht die Losungen an sich. Diese müssen von [www.losungen.de](http://www.losungen.de/download/) als `CSV (Tab getrennt)` heruntergeladen werden und im Modulverzeichnis als losungen.csv gespeichert werden.
--- losung.php
+++ losung.php
@@ -1,16 +1,23 @@
 <?php
-if (($handle = fopen("losungen.csv", "r")) !== FALSE) {
-	while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
-		if ($data[0] == date('d.m.Y')) {
-			$json = array(
-				"Sonntag" => htmlentities($data[2]),
-				"Losungsvers" => htmlentities($data[3]),
-				"Losungstext" => preg_replace('/\/(.*)\//', '<i>\1</i>', htmlentities($data[4])),
-				"Lehrtextvers" => htmlentities($data[5]),
-				"Lehrtext" => preg_replace('/\/(.*)\//', '<i>\1</i>', htmlentities($data[6])),
-			);
-			echo json_encode($json);
+if (file_exists("losungen.csv")) {
+	if (($handle = fopen("losungen.csv", "r")) !== FALSE) {
+		while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
+			if ($data[0] == date('d.m.Y')) {
+				$json = array(
+					"Sonntag" => htmlentities($data[2]),
+					"Losungsvers" => htmlentities($data[3]),
+					"Losungstext" => preg_replace('/\/(.*)\//', '<i>\1</i>', htmlentities($data[4])),
+					"Lehrtextvers" => htmlentities($data[5]),
+					"Lehrtext" => preg_replace('/\/(.*)\//', '<i>\1</i>', htmlentities($data[6])),
+				);
+				header('Content-Type: application/json');
+				echo json_encode($json);
+				break;
+			}
 		}
+		fclose($handle);
+		return '';
 	}
-	fclose($handle);
 }
+header('HTTP/1.1 500 Internal Server Error');
+echo 'failed to open losungen.csv';
